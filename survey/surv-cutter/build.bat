79ba03c99c822d6a89c3b2e41965a518baf640a6
Will Smith <will.smith@noaa.gov>
2019-02-27 15:46:14 -0800

    Remove old survey apps (moved to workspace) #148

--- survey/surv-cutter/build.bat
+++
@@ -1,3 +0,0 @@
-pushd ..
-electron-packager surv-cutter --platform=win32 --arch=x64 --overwrite --out=%userprofile%\Desktop --ignore="(\.history|\.vscode)"
-popd
--- survey/surv-cutter/src/app/_models/type/boatnet-date.ts
+++
@@ -1,5 +0,0 @@
-/**
- * BoatNet Date - Wrapper
- * ISO 8601 Date Type
- */
-type BoatnetDate = Date | string;
