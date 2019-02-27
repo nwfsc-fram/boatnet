301b99835255da96f549bdfc2e7be3951fd31f6b
Will Smith <will.smith@noaa.gov>
2019-02-27 13:41:18 -0800

    Create base survey apps (cutter, fpc, hm)

---
+++ workspace/apps/survey/surv-fpc/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'boatnet-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'survey-surv-fpc';
+}
