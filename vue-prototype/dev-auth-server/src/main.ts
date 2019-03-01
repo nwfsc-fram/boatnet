35774eb81d7c7d28d48547b7a4ff723f8005e08a
Will Smith <will.smith@noaa.gov>
2019-03-01 09:00:44 -0800

    Remove angular and nx dependencies from dev-auth-server (standalone) #153

--- vue-prototype/dev-auth-server/src/main.ts
+++ vue-prototype/dev-auth-server/src/main.ts
@@ -1,10 +1,57 @@
-interface Testing {
-  message: string;
-}
+// Dev Auth Server - simulates an auth server
+// For Boatnet app development use, without an actual auth endpoint.
 
-class TestStuff implements Testing {
-  message = 'Test typescript';
-}
+// FRAM Data Team 2019
 
-const thing = new TestStuff();
-console.log(thing.message);
+import express from 'express';
+import { Application } from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
+
+import * as bodyParser from 'body-parser';
+
+import { login } from './routes/login.route';
+import { pubkey } from './routes/pubkey.route';
+
+import { RSA_PRIVATE_KEY, RSA_CERT } from './util/security';
+
+const app: Application = express();
+
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [{ name: 'secure', type: Boolean }];
+
+const options = commandLineArgs(optionDefinitions);
+
+app.use(bodyParser.json()); // for parsing application/json
+
+// REST API
+// Login
+app.route('/api/login').post(login);
+// Public Key (dev use only)
+app.route('/api/pubkey').get(pubkey);
+
+if (options.secure) {
+  const httpsServer = https.createServer(
+    {
+      // Temporary Keys, not secret and publically shared
+      key: RSA_PRIVATE_KEY,
+      cert: RSA_CERT
+    },
+    app
+  );
+
+  // launch an HTTPS Server. Note: this does NOT mean that the application is secure
+  httpsServer.listen(9000, () => {
+    const address: any = httpsServer.address();
+    console.log(
+      'HTTPS Secure Server running at https://localhost:' + address.port
+    );
+  });
+} else {
+  // launch an HTTP Server
+  const httpServer = app.listen(3333, () => {
+    const address: any = httpServer.address();
+    console.log('HTTP Server running at http://localhost:' + address.port);
+  });
+}
---
+++ vue-prototype/dev-auth-server/src/routes/login.route.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from 'express';
+import {
+  createSessionToken,
+  createCsrfToken,
+  hashBoatnetPW
+} from '../util/security';
+const authConfig = require('../config/authProxyConfig.json');
+
+export async function login(req: Request, res: Response) {
+  const credentials = req.body;
+
+  const username = req.body.username || '';
+  const password = req.body.password || '';
+
+  if (username === '' || password === '') {
+    res.status(401);
+    res.json({
+      status: 401,
+      message: 'Invalid credentials - missing inputs.'
+    });
+    console.log('Missing user or pw.');
+    return;
+  }
+
+  const validate_result = await devValidateUserPw(username, password);
+  if (validate_result) {
+    console.log('Valid user result', validate_result);
+    const sessionToken = await createSessionToken(validate_result);
+    const csrfToken = await createCsrfToken();
+
+    console.log('Login successful');
+
+    res.cookie('SESSIONID', sessionToken, {
+      httpOnly: true,
+      secure: true
+    });
+
+    res.cookie('XSRF-TOKEN', csrfToken);
+
+    const result = {
+      user: validate_result.username,
+      token: sessionToken // You can check this token at https://jwt.io/
+    };
+
+    res.status(200).json(result);
+  } else {
+    res.status(401);
+    res.json({
+      status: 401,
+      message: 'Invalid credentials'
+    });
+    return;
+  }
+}
+
+async function devValidateUserPw(user: string, pw: string) {
+  /**
+   * Simplified user/pw validation for Development use
+   */
+  const users = <any>authConfig.devUsers;
+
+  let authedUser: any;
+  const isAuthed = users.some((u: any) => {
+    if (u.username === user && u.password === pw) {
+      authedUser = u;
+      return true;
+    }
+  });
+
+  if (isAuthed) {
+    console.log('User authorized: ', user);
+    const hashedPW = await hashBoatnetPW(authedUser.password);
+    return {
+      username: authedUser.username,
+      hashedPW: hashedPW,
+      roles: authedUser.userData.roles,
+      couchDBInfo: authedUser.userData.couchDBInfo
+    };
+  } else {
+    console.log('User not authorized: ', user);
+    return null;
+  }
+}
