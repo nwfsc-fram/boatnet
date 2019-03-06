adef85ec73cf79fc9adbf91e9227cb6308396bd8
Will Smith <will.smith@noaa.gov>
2019-03-05 16:20:26 -0800

    Add lerna workspace support

--- apps/obs-web/src/app/_models/message.ts
+++
@@ -1,9 +0,0 @@
-import { BoatnetBase } from './interface/boatnet-base';
-
-export class Message implements BoatnetBase {
-  id: string;
-  type: 'message';
-  created_by: string;
-  created_date: BoatnetDate;
-  content: string;
-}
