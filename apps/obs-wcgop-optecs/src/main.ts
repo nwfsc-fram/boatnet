34a052864b5c708df8b4216bee23aaf583c9b2b7
Will Smith <will.smith@noaa.gov>
2019-03-07 14:20:58 -0800

    Add Quasar 1.0.0 beta

--- apps/obs-wcgop-optecs/src/main.ts
+++ apps/obs-wcgop-optecs/src/main.ts
@@ -4,6 +4,47 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import './styles/quasar.styl';
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel
+} from 'quasar';
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel
+  },
+  directives: {},
+  plugins: {}
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
