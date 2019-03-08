edabc987bfea07821c4407e21794917c45065af6
Will Smith <will.smith@noaa.gov>
2019-03-08 09:06:37 -0800

    Fix Quasar css loading

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,7 @@
 // Created to fix Quasar build with lerna monorepo
 // https://github.com/quasarframework/quasar-extras/issues/3
+module.exports = {
+    plugins: {
+      autoprefixer: {}
+    }
+  }
