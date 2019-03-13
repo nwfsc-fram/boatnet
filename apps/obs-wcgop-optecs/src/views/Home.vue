8e8f147019f8b92d390a711ca9667f122fdd83fc
Will Smith <will.smith@noaa.gov>
2019-03-13 15:32:50 -0700

    Add GeoJSON support to bn-models.

--- apps/obs-wcgop-optecs/src/views/Home.vue
+++ apps/obs-wcgop-optecs/src/views/Home.vue
@@ -7,24 +7,34 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Point } from 'geojson';
 import {
   WcgopTrip,
   WcgopTripTypeName,
   Port,
   PortTypeName,
-  getNowDate
+  getNowDate,
+  WcgopHaul,
+  WcgopHaulTypeName,
+LocationEvent
 } from '@boatnet/bn-models';
 
 @Component
 export default class PageHome extends Vue {
-  private example: WcgopTrip;
+  private exampleHaul: WcgopHaul;
+  private exampleTrip: WcgopTrip;
+
 
   get currentProgram(): string | undefined {
-    return this.example ? this.example.program : '-';
+    return this.exampleTrip ? this.exampleTrip.program : '-';
   }
 
   get currentTrip(): string {
-    return this.example ? JSON.stringify(this.example) : '-';
+    return this.exampleTrip ? JSON.stringify(this.exampleTrip) : '-';
+  }
+
+  get currentHaul(): string {
+    return this.exampleHaul ? JSON.stringify(this.exampleHaul) : '-';
   }
 
   constructor() {
@@ -39,16 +49,42 @@ export default class PageHome extends Vue {
       name: 'Test Port'
     };
 
-    this.example = {
-      _id: 'fake-id-123',
-      type: WcgopTripTypeName,
-      program: 'Catch Shares',
+    const fakeLocation: LocationEvent = {
+      location: {  // GeoJSON https://tools.ietf.org/html/rfc7946
+        type: "Point",
+        coordinates: [124.6, 10.1]
+      },
+      date: getNowDate()
+    }
+    const fakeLocation2: LocationEvent = {
+      location: {
+        type: "Point",
+        coordinates: [122.6, 10.9]
+      },
+      date: getNowDate()
+    }
+
+    this.exampleHaul = {
+      _id: 'fake-haul-123',
+      type: WcgopHaulTypeName,
       createdBy: 'test',
       createdDate: getNowDate(),
+      haulNum: 1,
+      locations: [ fakeLocation, fakeLocation2 ]
+      // ... other data
+    }
+
+    this.exampleTrip = {
+      _id: 'fake-id-123',
+      type: WcgopTripTypeName,
+      createdBy: 'test',
+      createdDate: getNowDate(),
+      program: 'Catch Shares',
       departurePort: examplePort,
       departureDate: getNowDate(),
       returnPort: examplePort,
       returnDate: getNowDate(),
+      // ... other data
       legacy: {
         stuff: [1, 3, 4],
         other: 'test'
