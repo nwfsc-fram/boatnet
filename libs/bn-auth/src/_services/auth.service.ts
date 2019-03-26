e54dff56158770266cf54b36adcf818709b2150b
Will Smith <will.smith@noaa.gov>
2019-03-26 11:39:39 -0700

    Clean up auth service

--- libs/bn-auth/src/_services/auth.service.ts
+++ libs/bn-auth/src/_services/auth.service.ts
@@ -2,8 +2,6 @@
 
 /* tslint:disable:no-console */
 
-// Typescript examples https://codeburst.io/vuex-and-typescript-3427ba78cfa8
-
 import axios from 'axios';
 import cryptoJS from 'crypto-js';
 
@@ -24,7 +22,6 @@ async function validateUsername(username: string): Promise<boolean> {
   });
 }
 
-// TODO Refactor login routine commented out below
 async function login(username: string, password: string) {
   const pubKey = await getPubKey();
   const userResponse = await axios
@@ -52,24 +49,23 @@ async function login(username: string, password: string) {
         'Unable to log in using stored credentials. Internet connection required.'
       );
     } else {
-        const isStoredPwOK = checkPassword(storedUser, password);
-        if (!isStoredPwOK) {
-          throw new Error('Invalid offline username or password.');
-        } else {
-          setCurrentUser(storedUser.sub);
-        }
+      const isStoredPwOK = checkPassword(storedUser, password);
+      if (!isStoredPwOK) {
+        throw new Error('Invalid offline username or password.');
+      } else {
+        setCurrentUser(storedUser.sub);
+      }
     }
   }
 }
 
-// TODO Refactor logout routine commented out below
 function logout() {
-  // remove user from local storage to log user out
+  // TODO: Vuex store instead of localstorage
   localStorage.removeItem('user');
 }
 
 function setCurrentUser(user: BoatnetUser) {
-    // TODO: Vuex store instead of localstorage
+  // TODO: Vuex store instead of localstorage
   // store user details and jwt token in local storage to keep user logged in between page refreshes
   localStorage.setItem('user', JSON.stringify(user));
 }
@@ -85,7 +81,7 @@ function getCurrentUser(): BoatnetUser | undefined {
 }
 
 function isLoggedIn(): boolean {
-  // TODO Temp: use Vuex instead
+  // TODO: Vuex store instead of localstorage
   const logged = localStorage.getItem('user');
   return !!logged;
 }
@@ -154,110 +150,10 @@ function checkPassword(
   const salt = pwVals[0];
   const hashedPW = pwVals[1];
 
-  const hashedPwSHA = cryptoJS.SHA512(salt + password).toString(); // For FIPS compliance, need SHA-512 layer
+  const hashedPwSHA = cryptoJS.SHA512(salt + password).toString();
   const verified = hashedPW === hashedPwSHA;
   return verified;
 }
-
-function handleResponse(response: any) {
-  return response.text().then((text: any) => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      if (response.status === 401) {
-        // auto logout if 401 response returned from api
-        logout();
-        // location.reload(true);
-      }
-
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
-    }
-
-    return data;
-  });
-}
-
-/*
-  private loginKeyVerify(
-    username: string,
-    password: string,
-    pubKey: string
-  ): Observable<BoatnetUserToken> {
-    return this.http
-      .post<any>(this.authUrl + '/api/login', {
-        username: username,
-        password: password
-      })
-      .pipe(
-        map(result => {
-          const verified = jsonwebtoken.verify(result.token, pubKey);
-          this.authedUserToken = verified;
-          this.authedUserToken.sub = JSON.parse(verified.sub); // decode sub
-          this.storeUserToken(this.authedUserToken);
-          return this.authedUserToken;
-        }),
-        catchError(err => {
-          if (err.status === 401) {
-            return throwError('Invalid username or password.');
-          }
-          console.log('Getting stored user, because of error:', err);
-          const storedUser = this.getStoredUserToken(username);
-          if (!storedUser) {
-            return throwError(
-              'Unable to log in using cached credentials. Internet connection required.'
-            );
-          } else {
-            const isStoredPwOK = this.checkPassword(storedUser, password);
-            if (!isStoredPwOK) {
-              return throwError('Invalid offline username or password.');
-            } else {
-              this.authedUserToken = storedUser;
-              return of(this.authedUserToken);
-            }
-          }
-        }),
-        catchError(err => {
-          return throwError(err);
-        })
-      );
-  }
-
-
-
-  login(user: string, pw: string): Observable<BoatnetUser> {
-    // Login, and use cached credentials if unable to connect to login server.
-
-    return this.getPubKey().pipe(
-      // flattens Observable to its return value, exhaustMap only allows 1 login req. at a time
-      exhaustMap(key => this.loginKeyVerify(user, pw, key)),
-      map(result => {
-        return result.sub; // pull user out of JWT
-      }),
-      catchError(err => {
-        let errMsg = err;
-        if (err.error) {
-          errMsg = err.error.message;
-        }
-        return throwError(errMsg);
-      })
-    );
-  }
-
-  logout() {
-    console.log('[AuthService] TODO: Logout functionality.');
-    this.authedUserToken = undefined;
-    // TODO remove user from local storage to log user out
-    // localStorage.removeItem('currentUser');
-  }
-
-
-  public getCurrentUser(): BoatnetUser {
-    // TODO: Observable only?
-    return this.authedUserToken ? this.authedUserToken.sub : undefined;
-  }
-
-}
-*/
 
 export const authService = {
   validateUsername,
