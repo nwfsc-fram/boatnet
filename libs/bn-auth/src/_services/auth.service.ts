7159c501b1f080dd1a448c4003c774709f5f8247
Will Smith <will.smith@noaa.gov>
2019-04-01 15:15:09 -0700

    Refactor auth service to class

--- libs/bn-auth/src/_services/auth.service.ts
+++ libs/bn-auth/src/_services/auth.service.ts
@@ -12,160 +12,167 @@ import { BoatnetUserToken, BoatnetUser } from '../models/auth.model';
 
 import dbConfig from '../config/dbConfig';
 
-// TODO: Better username validation, maybe check for email address if we use email
-async function validateUsername(username: string): Promise<boolean> {
-  return new Promise((resolve, reject) => {
-    if (username.length > 3) {
-      resolve(true);
-    } else {
-      resolve(false);
-    }
-  });
-}
+class AuthService {
+  constructor() {
+    console.log('[Auth Service] Instantiated');
+  }
 
-async function login(username: string, password: string): Promise<BoatnetUser> {
-  const pubKey = await getPubKey();
-  const apiUrl = dbConfig && dbConfig.apiUrl ? dbConfig.apiUrl : '';
-  const userResponse = await axios
-    .post(apiUrl + '/api/v1/login', { username, password })
-    .catch((err) => {
-      console.log('[Auth Service]', err);
-      if (err.response && err.response.status === 401) {
-        throw new Error('Invalid username or password.');
-      }
-      // Else - possibly offline - Continue
-    });
-
-  if (userResponse) {
-    const user = userResponse.data;
-    const verified: any = jsonwebtoken.verify(user.token, pubKey!); // ! is the non-null assertion operator
-    verified.sub = JSON.parse(verified.sub); // parse JSON encoded sub
-    console.log('[Auth Service] Logged in as', verified.sub.username);
-    storeUserToken(verified);
-    setCurrentUser(verified.sub);
-    return verified.sub;
-  } else {
-    console.log('[Auth Service] Auth is Offline: Trying cached credentials');
-    const storedUser = getStoredUserToken(username);
-    if (!storedUser) {
-      throw new Error(
-        'Unable to log in using stored credentials. Internet connection required.'
-      );
-    } else {
-      const isStoredPwOK = checkPassword(storedUser, password);
-      if (!isStoredPwOK) {
-        throw new Error('Invalid offline username or password.');
+  // TODO: Better username validation, maybe check for email address if we use email
+  public async validateUsername(username: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      if (username.length > 3) {
+        resolve(true);
       } else {
-        setCurrentUser(storedUser.sub);
-        return storedUser.sub;
+        resolve(false);
+      }
+    });
+  }
+
+  public async login(username: string, password: string): Promise<BoatnetUser> {
+    const pubKey = await this.getPubKey();
+    const apiUrl = dbConfig && dbConfig.apiUrl ? dbConfig.apiUrl : '';
+    const userResponse = await axios
+      .post(apiUrl + '/api/v1/login', { username, password })
+      .catch((err) => {
+        console.log('[Auth Service]', err);
+        if (err.response && err.response.status === 401) {
+          throw new Error('Invalid username or password.');
+        }
+        // Else - possibly offline - Continue
+      });
+
+    if (userResponse) {
+      const user = userResponse.data;
+      const verified: any = jsonwebtoken.verify(user.token, pubKey!); // ! is the non-null assertion operator
+      verified.sub = JSON.parse(verified.sub); // parse JSON encoded sub
+      console.log('[Auth Service] Logged in as', verified.sub.username);
+      this.storeUserToken(verified);
+      this.setCurrentUser(verified.sub);
+      return verified.sub;
+    } else {
+      console.log('[Auth Service] Auth is Offline: Trying cached credentials');
+      const storedUser = this.getStoredUserToken(username);
+      if (!storedUser) {
+        throw new Error(
+          'Unable to log in using stored credentials. Internet connection required.'
+        );
+      } else {
+        const isStoredPwOK = this.checkPassword(storedUser, password);
+        if (!isStoredPwOK) {
+          throw new Error('Invalid offline username or password.');
+        } else {
+          this.setCurrentUser(storedUser.sub);
+          return storedUser.sub;
+        }
       }
     }
   }
-}
 
-function logout() {
-  // TODO: Vuex store instead of localstorage
-  console.log('[Auth Service] Logged out');
-  localStorage.removeItem('user');
-}
-
-function setCurrentUser(user: BoatnetUser) {
-  // TODO: Vuex store instead of localstorage
-  // store user details and jwt token in local storage to keep user logged in between page refreshes
-  localStorage.setItem('user', JSON.stringify(user));
-}
-
-function getCurrentUser(): BoatnetUser | null {
-  // TODO: Vuex store instead of localstorage
-  const userStored = localStorage.getItem('user');
-  let user: BoatnetUser | null;
-  if (userStored) {
-    user = JSON.parse(userStored);
-  } else {
-    user = null;
+  public logout() {
+    // TODO: Vuex store instead of localstorage
+    console.log('[Auth Service] Logged out');
+    localStorage.removeItem('user');
   }
-  return user;
-}
 
-function isLoggedIn(): boolean {
-  // TODO: Vuex store instead of localstorage
-  const logged = localStorage.getItem('user');
-  return !!logged;
-}
-
-async function getPubKey() {
-  // Gets public key for JWT verification.
-  // Pull from localstorage for offline mode
-  try {
-    const apiUrl = dbConfig && dbConfig.apiUrl ? dbConfig.apiUrl : '';
-    const result: any = await axios.get(apiUrl + '/api/v1/pubkey');
-    if (result.status === 200) {
-      const jwkKeyLoaded = result.data.keys[0]; // assuming our key is first
-      // TODO If we add multiple keys, we would use 'kid' property for matching
-      const pemKey = pemjwk.jwk2pem(jwkKeyLoaded);
-      localStorage.setItem('jwk-pub-key', JSON.stringify(jwkKeyLoaded));
-      return pemKey;
-    }
-  } catch (err) {
-    const jwkKey = localStorage.getItem('jwk-pub-key');
-    if (jwkKey) {
-      const storedJWK = JSON.parse(jwkKey);
-      console.log('[Auth Service] PEM key retrieved from localStorage.');
-      return pemjwk.jwk2pem(storedJWK);
+  public getCurrentUser(): BoatnetUser | null {
+    // TODO: Vuex store instead of localstorage
+    const userStored = localStorage.getItem('user');
+    let user: BoatnetUser | null;
+    if (userStored) {
+      user = JSON.parse(userStored);
     } else {
-      throw new Error(
-        'Public key not available. Internet connection required.'
-      );
+      user = null;
+    }
+    return user;
+  }
+
+  public isLoggedIn(): boolean {
+    // TODO: Vuex store instead of localstorage
+    const logged = localStorage.getItem('user');
+    return !!logged;
+  }
+
+  private setCurrentUser(user: BoatnetUser) {
+    // TODO: Vuex store instead of localstorage
+    // store user details and jwt token in local storage to keep user logged in between page refreshes
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  private async getPubKey() {
+    // Gets public key for JWT verification.
+    // Pull from localstorage for offline mode
+    try {
+      const apiUrl = dbConfig && dbConfig.apiUrl ? dbConfig.apiUrl : '';
+      const result: any = await axios.get(apiUrl + '/api/v1/pubkey');
+      if (result.status === 200) {
+        const jwkKeyLoaded = result.data.keys[0]; // assuming our key is first
+        // TODO If we add multiple keys, we would use 'kid' property for matching
+        const pemKey = pemjwk.jwk2pem(jwkKeyLoaded);
+        localStorage.setItem('jwk-pub-key', JSON.stringify(jwkKeyLoaded));
+        return pemKey;
+      }
+    } catch (err) {
+      const jwkKey = localStorage.getItem('jwk-pub-key');
+      if (jwkKey) {
+        const storedJWK = JSON.parse(jwkKey);
+        console.log('[Auth Service] PEM key retrieved from localStorage.');
+        return pemjwk.jwk2pem(storedJWK);
+      } else {
+        throw new Error(
+          'Public key not available. Internet connection required.'
+        );
+      }
     }
   }
-}
 
-function storePubKey(jwkKey: string) {
-  localStorage.setItem('jwk-pub-key', JSON.stringify(jwkKey));
-}
-
-function storeUserToken(userToken: BoatnetUserToken) {
-  const bnUsers = localStorage.getItem('bn-users');
-  let usersMap = new Map();
-  if (bnUsers) {
-    usersMap = new Map(JSON.parse(bnUsers));
+  private storePubKey(jwkKey: string) {
+    localStorage.setItem('jwk-pub-key', JSON.stringify(jwkKey));
   }
-  usersMap.set(userToken.sub.username, userToken);
-  localStorage.setItem('bn-users', JSON.stringify([...usersMap]));
-}
 
-function getStoredUserToken(username: string): BoatnetUserToken | undefined {
-  const bnUsers = localStorage.getItem('bn-users') || '';
-  if (bnUsers) {
-    const storedUsers = new Map(JSON.parse(bnUsers));
-    if (storedUsers && storedUsers.has(username)) {
-      return storedUsers.get(username) as BoatnetUserToken;
+  private storeUserToken(userToken: BoatnetUserToken) {
+    const bnUsers = localStorage.getItem('bn-users');
+    let usersMap = new Map();
+    if (bnUsers) {
+      usersMap = new Map(JSON.parse(bnUsers));
     }
-  }
-  return undefined;
-}
-
-function checkPassword(
-  storedUser: BoatnetUserToken,
-  password: string
-): boolean {
-  const pwVals = storedUser.sub.hashedPW.split('|');
-  if (pwVals.length !== 2) {
-    throw new Error('Error parsing PW string.');
+    usersMap.set(userToken.sub.username, userToken);
+    localStorage.setItem('bn-users', JSON.stringify([...usersMap]));
   }
 
-  const salt = pwVals[0];
-  const hashedPW = pwVals[1];
+  private getStoredUserToken(username: string): BoatnetUserToken | undefined {
+    const bnUsers = localStorage.getItem('bn-users') || '';
+    if (bnUsers) {
+      const storedUsers = new Map(JSON.parse(bnUsers));
+      if (storedUsers && storedUsers.has(username)) {
+        return storedUsers.get(username) as BoatnetUserToken;
+      }
+    }
+    return undefined;
+  }
 
-  const hashedPwSHA = cryptoJS.SHA512(salt + password).toString();
-  const verified = hashedPW === hashedPwSHA;
-  return verified;
+  private checkPassword(
+    storedUser: BoatnetUserToken,
+    password: string
+  ): boolean {
+    const pwVals = storedUser.sub.hashedPW.split('|');
+    if (pwVals.length !== 2) {
+      throw new Error('Error parsing PW string.');
+    }
+
+    const salt = pwVals[0];
+    const hashedPW = pwVals[1];
+
+    const hashedPwSHA = cryptoJS.SHA512(salt + password).toString();
+    const verified = hashedPW === hashedPwSHA;
+    return verified;
+  }
 }
 
-export const authService = {
-  validateUsername,
-  login,
-  logout,
-  getCurrentUser,
-  isLoggedIn
-};
+export const authService = new AuthService();
+// export const authService = {
+//   validateUsername,
+//   login,
+//   logout,
+//   getCurrentUser,
+//   isLoggedIn
+// };
