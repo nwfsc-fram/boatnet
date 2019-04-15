3e6591cf2d2b6c67ad7a43ac1207918f140cca51
Seth Gerou <seth.gerou@noaa.gov>
2019-04-15 09:52:01 -0700

    Obs-Web - minor tweaks

--- apps/obs-web/src/views/Home.vue
+++ apps/obs-web/src/views/Home.vue
@@ -3,23 +3,22 @@
     <img alt="noaa logo" src="../assets/NOAA_logo.svg" class="hero-logo">
 
     <div>
-    <q-btn @click="testCouch"> get stuff</q-btn>
     
     <q-select
-                    outlined
-                class="col-2"
-                v-model="myvessel"
-                label="Vessel Name/ Registration"
-                use-input
-                hide-selected
-                input-debounce="0"
-                :options="options"
-                @filter="filterFn">
-                <template v-slot:no-option>
-                  <q-item>
-                    <q-item-section class="text-grey">No results</q-item-section>
-                  </q-item>
-                </template>
+      outlined
+      class="col-2"
+      v-model="myvessel"
+      label="Vessel Name/ Registration"
+      use-input
+      hide-selected
+      input-debounce="0"
+      :options="options"
+      @filter="filterFn">
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">No results</q-item-section>
+        </q-item>
+      </template>
     </q-select>
     </div>
 
@@ -53,7 +52,7 @@ export default class Home extends Vue {
 
   }
 
-    private async filterFn(val: string, update: any, abort: any) {
+  private async filterFn(val: string, update: any, abort: any) {
     if (val.length < 2) {
       abort();
       return;
@@ -61,58 +60,54 @@ export default class Home extends Vue {
     update(async () => {
       try {
         const roDB: Client<any> = couchService.readonlyDB;
+
         const queryOptions: ListOptions = {
           limit: 5,
           start_key: val.toLowerCase(),
           inclusive_end: true,
           descending: false
         };
+
         const vessels = await roDB.view<any>(
           'optecs_trawl',
           'all_vessel_names',
           queryOptions
         );
+
         this.options = vessels.rows.map((vessel) => vessel.value);
       } catch (err) {
         this.error(err);
       }
     });
   }
-  private abortFilterFn() {
-    // console.log('delayed filter aborted');
-  }
 
-  private async testCouch() {
+  private async couch() {
+    const roDB: Client<any> = couchService.readonlyDB;
+    
+    const queryOptions: ListOptions = {
+          limit: 100,
+          start_key: 'a',
+          inclusive_end: true,
+          descending: false
+        };
+
     try {
-      // Lack of documentation, refer to options in code:
-      // https://github.com/nozzlegear/davenport/blob/master/index.ts
+        const vessels = await roDB.view<any>(
+          'optecs_trawl',
+          'all_vessel_names',
+          queryOptions
+        );
 
-      const userDB: Client<any> = couchService.userDB;
-      const roDB: Client<any> = couchService.readonlyDB;
+        this.options = vessels.rows.map((vessel) => vessel.value);
 
-      // Example:
-      // const singleDoc = await userDB.get<MyDocType>('489337588b5ff50b96779b7151001b7c');
-      const userStuff = await userDB.listWithDocs();
-      console.log(userStuff);
-
-      const options: ListOptions = {
-        limit: 10,
-        start_key: 'f',
-        end_key: 'x'
-      };
-
-      const vessels = await roDB.view<any>(
-        'optecs_trawl',
-        'all_vessel_names',
-        options
-      );
-      for (const v of vessels.rows) {
-        console.log(v.value);
-      }
-
+        console.log(this.options);
     } catch (err) {
       this.error(err);
     }
+  }
+
+  private created() {
+    this.couch();
   }
 
 }
