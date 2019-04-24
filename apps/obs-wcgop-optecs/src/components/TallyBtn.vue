bf71b121711195049b38491c255bb034b78bf596
Will Smith <will.smith@noaa.gov>
2019-04-24 10:54:44 -0700

    Create TallyBtn component (extend QBtn)

---
+++ apps/obs-wcgop-optecs/src/components/TallyBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-btn :color="color" :label="label"/>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
+import { WcgopAppState } from '../_store/types/types';
+import { State, Action } from 'vuex-class';
+import { QBtn } from 'quasar';
+
+@Component
+export default class TallyBtn extends Vue {
+  @Prop({default: undefined}) public color!: string;
+  @Prop({default: undefined}) public label!: string;
+}
+</script>
--- apps/obs-wcgop-optecs/src/views/Tally.vue
+++ apps/obs-wcgop-optecs/src/views/Tally.vue
@@ -1,5 +1,9 @@
 <template>
-<div>Tally</div>
+  <q-page padding>
+    <div class="q-gutter-md">
+      <tally-btn color="accent" label="Button" v-for="n in 40" :key="`md-${n}`"/>
+    </div>
+  </q-page>
 </template>
 
 
@@ -8,24 +12,20 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Tally extends Vue {
-
+  private horizButtonCount = 8;
+  private vertButtonCount = 4;
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style lang="stylus" scoped>
+.row > div {
+  padding: 10px 15px;
+  background: rgba(86, 61, 124, 0.15);
+  border: 1px solid rgba(86, 61, 124, 0.2);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.row + .row {
+  margin-top: 1rem;
 }
 </style>
