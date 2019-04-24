0094bf1e6864a33850cb0bcad0c80d1abbc7e905
Will Smith <will.smith@noaa.gov>
2019-04-24 10:13:14 -0700

    Fix build for .legacy tally app, create Tally.vue

---
+++ apps/obs-wcgop-optecs/src/views/Tally.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Tally extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- apps/obs-wcgop-optecs/src/views/Trips.vue
+++ apps/obs-wcgop-optecs/src/views/Trips.vue
@@ -86,7 +86,7 @@ export default class Trips extends Vue {
 
   private myStuff: any = {};
   private myPouchDB: any;
-  private vesselViewData: any;
+  private tempEmptyData: any[] = [];
 
   private userTrips!: any;
   constructor() {
@@ -257,10 +257,6 @@ export default class Trips extends Vue {
   private get lookupsDB() {
     // @ts-ignore
     return this[this.selectedDBName];
-  }
-
-  private get vesselView() {
-    return this.vesselViewData;
   }
 
   private displayKeyboard(e: any) {
