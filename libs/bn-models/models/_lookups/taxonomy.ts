34f296d09e145003b29c9a958e9fac2873c9bca9
Will Smith <will.smith@noaa.gov>
2019-05-03 17:56:49 -0700

    Fix whitespace and tslint warnings

--- libs/bn-models/models/_lookups/taxonomy.ts
+++ libs/bn-models/models/_lookups/taxonomy.ts
@@ -7,7 +7,6 @@ declare type TaxonomyLevel = string; // TODO Lookup
 export type TaxonomyId = CouchID;
 
 export interface Taxonomy extends Base {
-  
   taxonomyId: TaxonomyId; // Use this instead of _id for clarity except for top level record
   level: TaxonomyLevel;
   taxonomyName: string;
@@ -20,18 +19,18 @@ export interface Taxonomy extends Base {
   pacfinNomCode?: string; // when a landing does not have species comp
 
   // External system references
-  pacfinSpeciesId?: string; 
+  pacfinSpeciesId?: string;
   itisTSN?: number;
   wormsAphiaId?: number;
 
-  isInactive: boolean; 
+  isInactive: boolean;
 
   legacy?: {
     wcgopSpeciesId?: number; // primary key
     wcgopSpeciesCode?: number; // 3-4 digit code, adopted from AFSC
     wcgopCatchCategoryCode?: string; // ETL Note - only for single species catch category codes
     ashopSpeciesId?: number; // NORPAC ID
-    obsAnalystCode?: string // ToDo - Kayleigh spreadsheet
+    obsAnalystCode?: string; // ToDo - Kayleigh spreadsheet
     edcCode?: number;
     dwId?: number;
     raceBaseCodeNW?: number; // AFSC RaceBase code + 1 digit
