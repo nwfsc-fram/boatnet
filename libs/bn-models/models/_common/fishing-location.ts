a123372ec4c33669212bda95764dfbb3ee692146
Will Smith <will.smith@noaa.gov>
2019-05-30 10:34:19 -0700

    Fix various ts-lint and trailing whitespace errors

--- libs/bn-models/models/_common/fishing-location.ts
+++ libs/bn-models/models/_common/fishing-location.ts
@@ -2,17 +2,16 @@ import { Base } from '../_base/base';
 import { BoatnetDate, Measurement } from '.';
 import { Point } from 'geojson';
 
-export const FishingLocationTypeName = 'fishing-location'; 
+export const FishingLocationTypeName = 'fishing-location';
 
 export interface FishingLocation extends Base {
+  locationDate?: BoatnetDate;
+  location?: Point;
+  depth?: Measurement;
+  position?: number;
 
-    locationDate?: BoatnetDate
-    location?: Point;
-    depth?: Measurement;
-    position?: number;
-
-    legacy?: {
-        fishingLocationId?: number,
-        fishingActivityId?: number
-    }
+  legacy?: {
+    fishingLocationId?: number;
+    fishingActivityId?: number;
+  };
 }
