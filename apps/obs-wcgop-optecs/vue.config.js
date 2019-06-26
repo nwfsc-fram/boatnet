67f99b8b4dd804ee6ba05aeb6aef1e0ce6503066
Will Smith <will.smith@noaa.gov>
2019-06-26 16:57:39 -0700

    obs-web, obs-trips: remove publicPath until reverse proxy works

--- apps/obs-wcgop-optecs/vue.config.js
+++ apps/obs-wcgop-optecs/vue.config.js
@@ -8,5 +8,5 @@ module.exports = {
     }
   },
   transpileDependencies: [/[\\\/]node_modules[\\\/]quasar[\\\/]/],
-  publicPath: process.env.NODE_ENV === 'production' ? '/observer-trips/' : '/'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/observer-trips/' : '/'
 };
