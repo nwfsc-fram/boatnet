4a92a669230e90a88e1e837bf87a8135f4c96995
Will Smith <will.smith@noaa.gov>
2019-08-05 11:58:24 -0700

    Fix typescript warnings for taxonomy

--- libs/bn-models/models/_lookups/fishing-area.ts
+++ libs/bn-models/models/_lookups/fishing-area.ts
@@ -1,10 +1,10 @@
-import { MultiLineString } from "geojson";
+import { MultiLineString } from 'geojson';
 
 export const FishingAreaTypeName = 'fishing-area';
 
 // Source - OBSPROD.LOOKUPS.FISHING_AREA
 export interface FishingArea {
-  label: number;  // 100, 200, 300, 400
+  label: number; // 100, 200, 300, 400
   description: string;
   geography: MultiLineString; // Top + Bottom latitude lines
 }
