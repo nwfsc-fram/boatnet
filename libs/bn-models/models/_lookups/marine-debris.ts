7c126130b150923a5a152aa813d25ca4767e84e1
Todd Hay <todd.hay@noaa.gov>
2019-08-25 14:55:51 -0700

    Updated marine debris model

--- libs/bn-models/models/_lookups/marine-debris.ts
+++ libs/bn-models/models/_lookups/marine-debris.ts
@@ -1,6 +1,15 @@
 import { Base } from '../_base/index';
-import { BoatnetDate } from '../_common';
+import { Program } from './program';
+
+declare type MajorCategory = string; // TODO - Lookup - Define
+declare type MinorCategory = string; // TODO - Lookup - Define
 
 export const DebrisTypeName = 'debris';
 /* tslint:disable:no-empty-interface */
-export interface MarineDebris extends Base {}
+export interface MarineDebris extends Base {
+    majorCategory: MajorCategory;
+    minorCategory?: MinorCategory;
+    programs?: Program[];
+    isFishingRelated?: boolean;
+    isMilitaryRelated?: boolean;
+}
