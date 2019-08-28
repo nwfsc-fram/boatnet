d177e85917eaecfaf860a07aa9c812438501d1b9
Seth Gerou <seth.gerou@noaa.gov>
2019-08-28 12:50:39 -0700

    ots: remove non-functional service worker confirm.

--- apps/obs-web/src/views/Home.vue
+++ apps/obs-web/src/views/Home.vue
@@ -7,7 +7,6 @@
       </template>
     </q-banner>
     <img alt="noaa logo" src="../assets/NOAA_logo.svg" class="hero-logo">
-    <div class="fixed-bottom-right">one more test</div>
 
   </q-page>
 </template>
