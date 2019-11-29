8e6e133f8d48006a86604d1f1aeb905aaa1d850e
Seth Gerou <seth.gerou@noaa.gov>
2019-11-29 08:57:27 -0800

    obs-web: fix observer not required message on new trip

--- apps/obs-web/src/views/EMEFPManagement.vue
+++ apps/obs-web/src/views/EMEFPManagement.vue
@@ -109,7 +109,11 @@ private async getEmEfp() {
             queryOptions
             );
 
+        console.log(emefp);
+
         this.EM_EFP = emefp.rows.map((row: any) => row.doc);
+
+        console.log(this.EM_EFP);
 
     } catch (err) {
         this.error(err);
