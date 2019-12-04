890af255cb5a5123d9ff4c479db874eff2f23420
Will Smith <will.smith@noaa.gov>
2019-12-04 11:41:32 -0800

    Add bn-auth router configuration
    We will do this in all boatnet apps for Jenkins build purposes.

--- apps/obs-wcgop-optecs/src/config/dbConfig.ts
+++ apps/obs-wcgop-optecs/src/config/dbConfig.ts
@@ -1,10 +1,8 @@
-interface DBConfig {
-  apiUrl?: string;
-}
+import { DBConfig } from '@boatnet/bn-auth';
 
 // Leave unchanged for Dev - overwritten in Prod
 const dbConfig: DBConfig = {
-  // apiUrl: 'https://localhost:9000'
+  //apiUrl: 'https://localhost:9000'
 };
 
 export default dbConfig;
