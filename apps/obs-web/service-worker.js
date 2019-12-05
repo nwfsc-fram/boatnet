973e6c12962ea82d9a131a2b9c5517db1eb59e73
Seth Gerou <seth.gerou@noaa.gov>
2019-12-05 15:04:08 -0800

    obs-web: increment version number

--- apps/obs-web/service-worker.js
+++ apps/obs-web/service-worker.js
@@ -2,7 +2,7 @@
 
 workbox.core.setCacheNameDetails({ prefix: 'd4' })
 //Change this value every time before you build
-const LATEST_VERSION = 'v0.40'
+const LATEST_VERSION = 'v0.41'
 self.addEventListener('activate', (event) => {
   console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
   if (caches) {
