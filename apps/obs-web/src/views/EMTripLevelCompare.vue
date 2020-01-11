7c51dcff9ca52d5583e3060dfde6137d77cf12af
Seth Gerou <seth.gerou@noaa.gov>
2020-01-10 16:28:21 -0800

    ots: layout organization + EM Screens

---
+++ apps/obs-web/src/views/EMTripLevelCompare.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class='q-pa-md  q-gutter-md'>
+        <div>Hello {{ fish }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { createComponent, ref, reactive, computed, watch, onBeforeUnmount } from '@vue/composition-api';
+
+export default createComponent({
+    setup(props, context) {
+        const fish = context.root.$store.state.vessel.activeVessel.vesselName;
+        return {
+            fish
+        };
+
+    }
+});
+
+</script>
