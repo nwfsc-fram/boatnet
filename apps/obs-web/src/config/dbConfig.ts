b28d80503c04a7c3ccf5dc64b1dda3321706542f
Seth Gerou <seth.gerou@noaa.gov>
2020-03-19 15:01:07 -0700

    obs-web: increment version number

--- apps/obs-web/src/config/dbConfig.ts
+++ apps/obs-web/src/config/dbConfig.ts
@@ -3,6 +3,7 @@ import { DBConfig } from '@boatnet/bn-auth';
 // Leave unchanged for Dev - overwritten in Prod
 const dbConfig: DBConfig = {
   // apiUrl: 'https://localhost:9000'
+  apiUrl: 'https://nwcdevmeow1.nwfsc.noaa.gov:9000'
 };
 
 export default dbConfig;
