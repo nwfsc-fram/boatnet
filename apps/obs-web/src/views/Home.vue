2fc82beb83ce7bdc985cb20540b3bcebc2636836
Seth Gerou <seth.gerou@noaa.gov>
2020-06-09 12:21:24 -0700

    obs-web: #1616 - Logbook capture #1626 - Logbook capture: no option to load from photos or a file #1636 - Close trip dialog: captain view: move buttons

--- apps/obs-web/src/views/Home.vue
+++ apps/obs-web/src/views/Home.vue
@@ -92,8 +92,6 @@
 
         <q-btn v-if="isAuthorized(['captain', 'staff', 'debriefer'])" label="Trips" to="/trips" color="primary" exact style="margin: 5px"></q-btn>
 
-        <q-btn v-if="isAuthorized(['development_staff', 'staff', 'data_steward', 'program_manager', 'coordinator']) && !user.captainMode" label="E Logbook" to="/e-logbook/new" color="primary" exact style="margin: 5px"></q-btn>
-
         <q-btn label="My Details" to="/user-config" color="primary" exact style="margin: 5px"></q-btn>
       <br>
       <q-toggle v-if="isAuthorized(['development_staff', 'staff', 'data_steward', 'program_manager', 'coordinator'])" v-model="user.captainMode" label="Captain Mode" @input="enableCaptainMode" style="margin-top: 30px;"/>
--- apps/obs-web/src/views/Trips.vue
+++ apps/obs-web/src/views/Trips.vue
@@ -19,6 +19,7 @@
       <div v-if="vessel.activeVessel">
         <q-btn class="bg-primary text-white q-ma-md" v-if="openTrips.length < 2 && !loading" color="primary" @click="newTrip">New Trip</q-btn>
         <q-btn v-else color="blue-grey-2" class="q-ma-md" @click="maxTripsAlert = true">New Trip</q-btn>
+        <q-btn class="bg-secondary text-white q-ma-md float-right" color="secondary" :to="'/log-missing-trip'" label="log missed trip"></q-btn>
       </div>
       <div v-else>
         <p>No active vessel</p>
