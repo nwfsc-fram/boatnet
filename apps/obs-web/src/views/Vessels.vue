0f72aefcf50f5da7a3fba793ce1a3f19fd3135da
Seth Gerou <seth.gerou@noaa.gov>
2020-06-11 16:37:58 -0700

    obs-web: #1630 - home ports missing from vessels table #1667 - global search in vessel mgmt

--- apps/obs-web/src/views/Vessels.vue
+++ apps/obs-web/src/views/Vessels.vue
@@ -21,16 +21,16 @@
             :selected.sync="selected"
             :pagination.sync="pagination"
             :loading="loading"
-            :filter="vessel.filterText"
-            @request="onRequest"
             binary-state-sort
             hide-bottom
         >
 
+            <!-- :filter="vessel.filterText"
+            @request="onRequest" -->
             <template v-slot:top>
-                <q-input label="Vessel Name" v-model="vessel.filterText" debounce="300" placeholder="Search" style="width:100%" autofocus >
+                <q-input label="Vessel Name or Id" v-model="vessel.filterText" debounce="1000" placeholder="Search" style="width:100%" autofocus>
                     <template v-if="vessel.filterText">
-                        <q-avatar dense icon="clear" @click="vessel.filterText = ''"></q-avatar>
+                        <q-avatar dense icon="close" @click="vessel.filterText = ''"></q-avatar>
                     </template>
                 </q-input>
             </template>
@@ -41,8 +41,8 @@
                     <q-td key="vesselName" :props="props">{{ props.row.vesselName }}</q-td>
                     <q-td key="vesselCGNumber" :props="props">{{ (props.row.coastGuardNumber ? props.row.coastGuardNumber : props.row.stateRegulationNumber) }}</q-td>
                     <q-td key="vesselType" :props="props">{{ props.row.vesselType ? props.row.vesselType.description : '' }}</q-td>
-                    <q-td key="registeredLength" :props="props">{{ props.row.registeredLength.value ? props.row.registeredLength.value : '' }}</q-td>
-                    <q-td key="port" :props="props">{{ props.row.port ? props.row.port.name : '' }}</q-td>
+                    <q-td key="registeredLength" :props="props">{{ props.row.registeredLength ? props.row.registeredLength.value : '' }}</q-td>
+                    <q-td key="port" :props="props">{{ props.row.homePort ? props.row.homePort.name : '' }}</q-td>
                     <q-td key="isActive" :props="props">{{ props.row.isActive ? 'Active' : 'Inactive' }}</q-td>
                     <q-td key="notes" :props="props">{{ props.row.notes }}</q-td>
                     <q-td key="emHardware" :props="props">{{ props.row.emHardware ? props.row.emHardware.description : '' }}</q-td>
@@ -89,7 +89,9 @@ private pagination = {
     };
 
 private vessels: Vessel[] = [];
+private vesselNames: any = [];
 private loading: boolean = false;
+private searchText: string = '';
 
 private columns = [
     {name: 'vesselName', label: 'Vessel Name', field: 'vesselName', required: true, align: 'left', sortable: true },
@@ -97,7 +99,7 @@ private columns = [
     sortable: true, align: 'left' },
     {name: 'vesselType', label: 'Vessel Type', field: 'vesselType', required: true, align: 'left', sortable: true },
     {name: 'registeredLength', label: 'Registered Length (ft)', field: 'registeredLength', required: true, align: 'left', sortable: true },
-    {name: 'port', label: 'Port', field: 'port', required: true, align: 'left', sortable: true },
+    {name: 'port', label: 'Port', field: 'Home Port', required: true, align: 'left', sortable: true },
     {name: 'isActive', label: 'Status', field: 'isActive', required: true, align: 'left', sortable: true },
     {name: 'notes', label: 'Notes', field: 'notes', required: true, align: 'left', sortable: false },
     {name: 'emHardware', label: 'EM Hardware', field: 'emHardware', required: false, align: 'left', sortable: true },
@@ -110,15 +112,20 @@ private async getVessels() {
     try {
         this.loading = true;
         const queryOptions: any = {
-          include_docs: true
+          include_docs: false
         };
 
-        const vessels = await masterDB.viewWithDocs<any>(
+        const vessels = await masterDB.view<any>(
             'obs_web',
             'all_vessel_names',
             queryOptions
             );
 
+
+        this.vesselNames = vessels.rows.map( (row: any) => {
+            return {vesselNameAndReg: row.value, id: row.id};
+        });
+
         this.loading = false;
 
     } catch (err) {
@@ -126,51 +133,60 @@ private async getVessels() {
     }
   }
 
-  private async onRequest(props: any) {
+//   private async onRequest(props: any) {
 
-    const { page, rowsPerPage, rowsNumber, sortBy, descending } = props.pagination;
-    const filter = props.filter ? props.filter : '';
+//     const { page, rowsPerPage, rowsNumber, sortBy, descending } = props.pagination;
+//     const filter = props.filter ? props.filter : '';
 
-    try {
-        this.loading = true;
-        const db = couchService.masterDB;
-        const queryOptions = {
-        limit: 20,
-        start_key: filter.toLowerCase(),
-        end_key: filter.toLowerCase() + '\u9999' ,
-        inclusive_end: true,
-        descending: false,
-        include_docs: true
-        };
+//     try {
+//         this.loading = true;
+//         const db = couchService.masterDB;
+//         const queryOptions = {
+//             limit: 20,
+//             start_key: filter.toLowerCase(),
+//             end_key: filter.toLowerCase() + '\u9999' ,
+//             inclusive_end: true,
+//             descending: false,
+//             include_docs: true
+//         };
 
-        const vessels = await db.viewWithDocs(
-            'obs_web',
-            'all_vessels',
-            queryOptions
-            );
+//         const vessels = await db.view(
+//             'obs_web',
+//             'all_vessel_names',
+//             queryOptions
+//             );
 
-        for (const row of vessels.rows) {
-            if ( row.doc.isActive === undefined ) {
-                row.doc.isActive = true;
-            }
-        }
+//         for (const row of vessels.rows) {
+//             if ( row.doc.isActive === undefined ) {
+//                 row.doc.isActive = true;
+//             }
+//         }
 
-        this.vessels = vessels.rows.map((row: any) => row.doc);
+//         this.vessels = vessels.rows.map((row: any) => row.doc);
 
-        this.loading = false;
-        return this.vessels;
-    } catch (err) {
-        this.errorAlert(err);
-    }
+//         this.loading = false;
+//         return this.vessels;
+//     } catch (err) {
+//         this.errorAlert(err);
+//     }
 
-  }
+//   }
 
   private created() {
-    // this.getVessels();
-    this.onRequest({
-        pagination: this.pagination,
-        filter: this.vessel.filterText
-    });
+    this.getVessels().then(
+        () => {
+            if (this.vessel.filterText) {
+                return this.getSearchVessels(this.vessel.filterText);
+            } else {
+               return this.getSearchVessels('a');
+            }
+        }
+    );
+
+    // this.onRequest({
+    //     pagination: this.pagination,
+    //     filter: this.vessel.filterText
+    // });
   }
 
 private vesselDetails(vessel: any) {
@@ -197,6 +213,40 @@ private newVessel() {
     };
     this.$router.push({path: '/vessels/' + 'new'});
 }
+
+  private async getSearchVessels(searchString: string) {
+    this.vessels = [];
+    const matchedVessels = this.vesselNames.filter((vessel: any) => vessel.vesselNameAndReg.toLowerCase().includes(searchString.toLowerCase()));
+    matchedVessels.sort((a: any, b: any) => {
+        const keyA = a.vesselNameAndReg.toLowerCase();
+        const keyB = b.vesselNameAndReg.toLowerCase();
+        if (keyA < keyB) {
+            return -1;
+        }
+        if (keyA > keyB) {
+            return 1;
+        }
+        return 0;
+        }
+    )
+
+    const ids = matchedVessels.map( (vessel: any) => vessel.id);
+    try {
+        const db = couchService.masterDB;
+        for (const id of ids.splice(0, 20)) {
+            const doc = await db.get(id);
+            this.vessels.push(doc);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+  }
+
+  @Watch('vessel.filterText')
+  private async handler3(newVal: string, oldVal: string) {
+    this.getSearchVessels(newVal);
+  }
 
 }
 
