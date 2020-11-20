b128d1db3d2ccf3b888dd24b326e295d5f5e5449
Seth Gerou <seth.gerou@noaa.gov>
2020-11-20 14:17:00 -0800

    em: footage manager

--- apps/obs-web/src/views/EMFootageManager.vue
+++ apps/obs-web/src/views/EMFootageManager.vue
@@ -1,38 +1,50 @@
 <template>
   <div class="q-pa-md q-gutter-md">
+    <q-btn
+      color="primary"
+      label="new"
+      @click="newFootage"
+    >
+    </q-btn>
 
-        <q-table
-            :data="footage"
-            :columns="columns"
-            dense
-            row-key="id"
-            selection="single"
-            :selected.sync="selected"
-            :pagination.sync="pagination"
-            binary-state-sort
-            hide-bottom
-        >
+    <q-table
+        :data="filteredFootage"
+        :columns="columns"
+        dense
+        row-key="id"
+        selection="single"
+        :selected.sync="selected"
+        :pagination.sync="pagination"
+        binary-state-sort
+        hide-bottom
+    >
+      <template v-slot:top>
+          <q-input
+            label="tripNum"
+            v-model="filterText"
+            debounce="2000"
+            placeholder="Search"
+            style="width:100%"
+            autofocus
+            :rules="[val => val.length <= 6 || 'tripNum is a 6 digit number']"
+          >
+              <template v-if="filterText">
+                  <q-avatar dense icon="close" @click="filterText = ''"></q-avatar>
+              </template>
+          </q-input>
+      </template>
 
-            <template v-slot:top>
-                <q-input label="tripNum" v-model="tripFilter" debounce="1000" placeholder="Search" style="width:100%" autofocus>
-                    <template v-if="tripFilter">
-                        <q-avatar dense icon="close" @click="tripFilter = ''"></q-avatar>
-                    </template>
-                </q-input>
-            </template>
-
-            <template v-slot:body="props">
-                <q-tr :props="props" @click.native="footageDetails(props.row)">
-                    <q-td key="_id"></q-td>
-                    <q-td key="tripNum" :props="props">{{ props.row.tripNum }}</q-td>
-                    <q-td key="vesselName" :props="props">{{ props.row.vesselName }}</q-td>
-                    <q-td key="vesselId" :props="props">{{ props.row.vesselId }}</q-td>
-                    <q-td key="dateAdded" :props="props">{{ props.row.dateAdded }}</q-td>
-                    <q-td key="filesLocation" :props="props">{{ props.row.filesLocation }}</q-td>
-                </q-tr>
-            </template>
-
-        </q-table>
+      <template v-slot:body="props">
+          <q-tr :props="props" @click.native="footageDetails(props.row)">
+              <q-td key="_id"></q-td>
+              <q-td key="tripNum" :props="props">{{ props.row.tripNum }}</q-td>
+              <q-td key="vesselName" :props="props">{{ props.row.vesselName }}</q-td>
+              <q-td key="vesselId" :props="props">{{ props.row.vesselId }}</q-td>
+              <q-td key="footageAdded" :props="props">{{ props.row.footageAdded ? formatDate(props.row.footageAdded) : '' }}</q-td>
+              <q-td key="filesLocation" :props="props">{{ props.row.filesLocation }}</q-td>
+          </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
 
@@ -57,6 +69,7 @@ import {
 import { Client, CouchDoc, ListOptions } from 'davenport';
 
 import { Notify } from 'quasar';
+import moment from 'moment';
 
 import { getTripsApiTrip, getCatchApiCatch } from '@boatnet/bn-common';
 
@@ -65,29 +78,69 @@ export default createComponent({
     const store = context.root.$store;
     const state = store.state;
 
-    const tripFilter: any = ref(null);
+    const filterText: any = ref('');
 
     const columns = [
             {name: 'tripNum', label: 'Trip #', field: 'tripNum', required: false, align: 'left', sortable: true},
             {name: 'vesselName', label: 'Vessel Name', field: 'vesselName', required: false, align: 'left', sortable: true},
             {name: 'vesselId', label: 'Vessel Id', field: 'vesselId', required: false, align: 'left', sortable: true},
-            {name: 'dateAdded', label: 'Date Added', field: 'dateAdded', required: false, align: 'left', sortable: true},
+            {name: 'footageAdded', label: 'Date Added', field: 'footageAdded', required: false, align: 'left', sortable: true},
             {name: 'filesLocation', label: 'Files Location', field: 'filesLocation', required: false, align: 'left', sortable: true}
     ];
 
     const selected: any = [];
     const pagination = {sortBy: 'tripNum', descending: true, rowsPerPage: 40};
 
-    const footage = [
-      {_id: 'erfgjk34h52w34kjtrwhergkljfw34nrtqe', tripNum: '123334', vesselName: 'Fish Master 3000', vesselId: '0G814', dateAdded: '2020-03-05T11:32:53-08:00', filesLocation: 'V://directory/subdirectory/filesfolder'}
-    ];
+    const masterDB: Client<any> = couchService.masterDB;
+
+    const footageDocs: any[] = reactive([]);
 
     const footageDetails = (row: any) => {
       context.root.$router.push({path: '/em-footage-detail/' + row.tripNum});
     };
 
+    const newFootage = (row: any) => {
+      context.root.$router.push({path: '/em-footage-detail/new'});
+    };
+
+    const formatDate: any = (dateVal: any) => {
+      return moment(dateVal).format('MMM DD, YYYY HH:mm');
+    };
+
+    const filteredFootage = computed(
+      () => {
+        if (filterText.value.length <= 6 && filterText.value.length > 0) {
+          return footageDocs.filter( (doc: any) => {
+            return doc.tripNum.includes(filterText.value);
+          });
+        } else {
+          return footageDocs;
+        }
+      }
+    )
+
+    onMounted( async () => {
+      const footageQuery = await masterDB.view(
+        'TripsApi',
+        'all_footage_location',
+        {reduce: false, include_docs: false} as any
+      );
+      if (footageQuery.rows.length > 0) {
+        footageQuery.rows.forEach( (row: any) => {
+          footageDocs.push({
+                  _id: row.id,
+                  tripNum: row.key,
+                  vesselName: row.value[0],
+                  vesselId: row.value[1],
+                  footageAdded: row.value[2],
+                  filesLocation: row.value[3]
+                });
+        });
+      }
+    });
+
     return {
-        columns, footage, selected, pagination, footageDetails, tripFilter
+        columns, footageDocs, selected, pagination, footageDetails, filterText, newFootage, formatDate, filteredFootage
     };
   }
 });
