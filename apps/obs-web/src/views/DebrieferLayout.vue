aa570e2923a54f4f1325cd4ce9a9cd6989c82b67
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2021-02-09 17:36:52 -0800

    accidentially committed maximizable button, getting rid of that

--- apps/obs-web/src/views/DebrieferLayout.vue
+++ apps/obs-web/src/views/DebrieferLayout.vue
@@ -33,16 +33,6 @@
       <TabPanel header="Data" :active="infoTab === 'data'">
         <app-debriefer-wcgop-data v-if="program === 'wcgop'" startingTab="trips" :isFullSize="false"/>
         <app-debriefer-ashop-data v-else />
-        <h5>Maximizable</h5>
-    <Button label="Show" icon="pi pi-external-link" @click="openMaximizable" />
-    <Dialog header="Header" :visible="displayMaximizable" :style="{width: '50vw'}" :maximizable="true" :modal="true">
-        <app-debriefer-wcgop-data v-if="program === 'wcgop'" startingTab="trips" :isFullSize="false"/>
-
-    <template #footer>
-        <Button label="No" icon="pi pi-times" @click="closeMaximizable" class="p-button-text"/>
-        <Button label="Yes" icon="pi pi-check" @click="closeMaximizable" autofocus />
-    </template>
-</Dialog>
       </TabPanel>
       <TabPanel header="Errors" :active="infoTab === 'qa'">
         <app-debriefer-errors :showData="infoTab === 'qa' ? false : true" />
@@ -56,24 +46,16 @@
 </template>
 
 <script lang="ts">
-import {
-  createComponent,
-  ref,
-  reactive,
-  computed,
-  watch
-} from '@vue/composition-api';
+import { createComponent, ref, computed } from '@vue/composition-api';
 import Vue from 'vue';
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import DebrieferSelectComp from './DebrieferSelectComp.vue';
-import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
 
 Vue.component('Button', Button);
-Vue.component('Dialog', Dialog);
 Vue.component('DebrieferSelectComp', DebrieferSelectComp);
 Vue.component('TabPanel', TabPanel);
 Vue.component('TabView', TabView);
@@ -85,12 +67,9 @@ export default createComponent({
     const store = context.root.$store;
     const state = store.state;
     const infoTab = ref('data');
-    const dialog: any = ref(false);
-    const maximizedToggle: any = ref(true);
 
     const show: any = ref(true);
     const activeParamsTab: any = ref('evaluation');
-    const displayMaximizable: any = ref(false);
 
     function toggleShow(label: string) {
       const prevActiveTab = activeParamsTab.value;
@@ -121,26 +100,13 @@ export default createComponent({
       store.dispatch('debriefer/updateEvaluationPeriod', {});
     }
 
-    function  openMaximizable() {
-      displayMaximizable.value = true;
-    }
-
-    function closeMaximizable() {
-      displayMaximizable.value = false;
-    }
-
     return {
       program,
       infoTab,
       clearFilters,
-      dialog,
-      maximizedToggle,
       toggleShow,
       show,
-      activeParamsTab,
-      displayMaximizable,
-      openMaximizable,
-      closeMaximizable
+      activeParamsTab
     };
   }
 });
