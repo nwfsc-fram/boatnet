06b6ce589541f8a1687204f2599f60351e8a8104
Seth Gerou <seth.gerou@noaa.gov>
2021-04-02 08:52:20 -0700

    debriefer: create assessment per evaluation period #2237

--- apps/obs-web/src/views/DebrieferAssessment.vue
+++ apps/obs-web/src/views/DebrieferAssessment.vue
@@ -1,49 +1,46 @@
 <template>
-    <div>
+    <div v-if="assessment">
         <q-card class="assessment-section">
             <q-card-section>
-                <p style="font-weight: bold; font-size: 20px; letter-spacing: .5px;">Fishing Effort:</p>
+                <p style="font-weight: bold; font-size: 20px; letter-spacing: .5px;" class="text-primary">Fishing Effort:</p>
                 <div>
-                    # vessels: {{ uniqueVessels }}<br>
-                    # Trips: {{ debriefer.trips.length }}<br>
-                    # Hauls: {{ totalHauls }}
+                    Vessels: <b>{{ uniqueVessels }}</b>&nbsp;
+                    Trips: <b>{{ debriefer.trips.length }}</b>&nbsp;
+                    Hauls: <b>{{ totalHauls }}</b>
                 </div>
             </q-card-section>
         </q-card>
         <q-card v-for="section of sections" :key="sections.indexOf(section)" class="assessment-section">
             <q-card-section>
-                <p style="font-weight: bold; font-size: 20px; letter-spacing: .5px;">{{section}}</p>
-                <div v-for="question of questions" :key="questions.indexOf(question)">
-                    <div v-if="question.section === section" class="row">
-                        <span class="col5" style="position: relative; top: 10px; width: 400px;">{{ question.question }}&nbsp; &nbsp;</span>
+                <p style="font-weight: bold; font-size: 20px; letter-spacing: .5px;" class="text-primary">{{section}}</p>
+                <div v-for="(assessmentResponse, i) of assessment.assessmentResponses" :key="i">
+                    <div v-if="assessmentResponse.question.section === section" class="row">
+                        <span class="col5" style="position: relative; top: 10px; width: 400px;">{{ assessmentResponse.question.question }}&nbsp; &nbsp;</span>
                         <div class="col">
                             <q-input
-                                v-model="assessment[section + ' || ' +  question.question]"
-                                autogrow
-                                dense
+                                v-model="assessmentResponse.response"
+                                autogrow dense
                             >
-                            <q-menu anchor="top left" style="cursor: pointer">
-                                <q-list dense >
-                                    <q-item
-                                        v-for="answer of getAnswerSet(question.answerSet)"
-                                        :key="getAnswerSet(question.answerSet).indexOf(answer)"
-                                        clickable
-                                        @click="setQuestionResponse(section + ' || ' +  question.question, answer)"
-                                        v-close-popup>
-                                        <q-item-section>{{ answer }}</q-item-section>
-                                    </q-item>
-                                </q-list>
-                            </q-menu>
-                            <template v-slot:append>
-                                <q-btn
-                                    v-if="assessment[section + ' || ' +  question.question]"
-                                    icon="clear"
-                                    size="xs"
-                                    flat
-                                    round
-                                    @click="setQuestionResponse(section + ' || ' +  question.question, null)"
-                                ></q-btn>
-                            </template>
+                                <q-menu anchor="top left" style="cursor: pointer">
+                                    <q-list dense >
+                                        <q-item
+                                            v-for="answer of getAnswerSet(assessmentResponse.question.answerSet)"
+                                            :key="getAnswerSet(assessmentResponse.question.answerSet).indexOf(answer)"
+                                            clickable
+                                            @click="setQuestionResponse(i, answer)"
+                                            v-close-popup
+                                            style="max-width: 500px">
+                                            <q-item-section>{{ answer }}</q-item-section>
+                                        </q-item>
+                                    </q-list>
+                                </q-menu>
+                                <template v-slot:append>
+                                    <q-btn
+                                        v-if="assessment.assessmentResponses[i].response"
+                                        icon="clear" size="xs" flat round
+                                        @click="setQuestionResponse(i, null)"
+                                    ></q-btn>
+                                </template>
                             </q-input>
                         </div>
                     </div>
@@ -59,13 +56,14 @@ import {
   ref,
   onMounted,
   watch,
-  reactive,
   set
 } from '@vue/composition-api';
 
 import { couchService } from '@boatnet/bn-couch';
 import { Client } from 'davenport';
-import {  orderBy, uniq } from 'lodash';
+import { orderBy, uniq } from 'lodash';
+import moment from 'moment';
+import { authService } from '@boatnet/bn-auth';
 
 export default createComponent({
   props: {
@@ -98,7 +96,7 @@ export default createComponent({
         'Requirements for Return (Exit/Year-end only)',
         'Summary'
     ];
-    const assessment: any = ref({});
+    const assessment: any = ref(null);
 
     const getAnswerSet = (setName: string) => {
         const answerSet = answerSets.value.find( (row: any) => row.name === setName );
@@ -148,17 +146,84 @@ export default createComponent({
         getTotalHauls();
     };
 
-    const setQuestionResponse = (question: any, answer: any) => {
-        set(assessment.value, question, answer);
+    const getOrCreateAssesment = async () => {
+        if (debriefer.evaluationPeriod && debriefer.evaluationPeriod.id) {
+            const assessmentQuery: any = await masterDB.view(
+                'obs_web-new',
+                'observer-assessments',
+                {include_docs: true, key: debriefer.evaluationPeriod.id} as any
+            );
+            const assessments = assessmentQuery.rows.map( (row: any) => row.doc);
+            if (assessments[0]) {
+                assessment.value = assessments[0];
+            } else {
+                const newAssessment: any = {
+                    type: 'observer-assessment',
+                    evaluationPeriod: debriefer.evaluationPeriod.id,
+                    createdBy: authService.getCurrentUser() ? authService.getCurrentUser()!.username : '',
+                    createdDate: moment().format(),
+                    assessmentResponses: []
+                };
+                for (const question of questions.value) {
+                    newAssessment.assessmentResponses.push(
+                        {
+                            question,
+                            response: null
+                        }
+                    );
+                }
+                await masterDB.post(
+                    newAssessment
+                ).then( async (response) => {
+                    assessment.value = await masterDB.get(response.id);
+                });
+            }
+        } else {
+            assessment.value = null;
+        }
+    };
+
+    const save = async () => {
+        assessment.value.updatedBy = authService.getCurrentUser()!.username;
+        assessment.value.updaedDate = moment().format();
+        assessment.value.vesselCount = uniqueVessels.value;
+        assessment.value.tripCount = debriefer.trips.length;
+        assessment.value.haulCount = totalHauls.value;
+
+        await masterDB.put(
+            assessment.value._id,
+            assessment.value,
+            assessment.value._rev
+        ).then(
+            (response) => {
+                assessment.value._rev = response.rev;
+            }
+        );
+    };
+
+    const setQuestionResponse = (index: number, answer: any) => {
+        set(assessment.value.assessmentResponses[index], 'response', answer);
     };
 
     onMounted( async () => {
-        getQuestions();
-        getAnswerSets();
+        if (!debriefer.observer) {
+            store.dispatch('debriefer/updateEvaluationPeriod', {});
+        }
         getUniqueVessels();
+        getQuestions().then(
+            () => getOrCreateAssesment()
+        );
+        getAnswerSets();
     });
 
     watch(() => state.debriefer.trips, getUniqueVessels);
+    watch(() => assessment.value.assessmentResponses, (newVal, oldVal) => {
+      if (oldVal) {
+        save();
+      }
+    },
+    {lazy: true, deep: true});
+    watch(() => state.debriefer.evaluationPeriod, getOrCreateAssesment, {deep: true});
 
     return {
         answerSets,
