a3804d7757c676a74af52a643a0eab46e35fc8a1
Seth Gerou <seth.gerou@noaa.gov>
2021-06-18 16:06:09 -0700

    obs-web: vessel selections contd fish tickets view

---
+++ apps/obs-web/src/views/FishTickets.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="flex flex-center q-pa-md  q-gutter-md">
+        {{ ftids }}
+
+        <q-table
+            :data="fishTickets"
+            :columns="ftColumns"
+            dense
+            row-key="id"
+            selection="single"
+            :selected.sync="selected"
+            :pagination.sync="pagination"
+            :loading="ftLoading"
+            binary-state-sort
+            style="width: 100%"
+        >
+
+            <template v-slot:body="props">
+                <q-tr :props="props">
+                    <q-td key="id"></q-td>
+                    <q-td key="FTID" :props="props">{{ props.row.FTID }}</q-td>
+                    <q-td key="VESSEL_NUM" :props="props">{{ props.row.VESSEL_NUM }}</q-td>
+                    <q-td key="CONDITION_CODE" :props="props">{{ props.row.CONDITION_CODE = 'R' ? 'Retained' : 'Discarded' }}</q-td>
+                    <q-td key="SPECIES_NAME" :props="props">{{ props.row.SPECIES_NAME }}</q-td>
+                    <q-td key="LANDED_WEIGHT_LBS" :props="props">{{ props.row.LANDED_WEIGHT_LBS }}</q-td>
+                    <q-td key="NUM_OF_FISH" :props="props">{{ props.row.NUM_OF_FISH }}</q-td>
+                </q-tr>
+            </template>
+
+        </q-table>
+
+    </div>
+</template>
+
+<script lang="ts">
+import {
+  createComponent,
+  ref,
+  reactive,
+  computed,
+  watch,
+  onMounted
+} from '@vue/composition-api';
+
+import { Vue, Watch } from 'vue-property-decorator';
+import { CouchDBInfo, CouchDBCredentials, couchService } from '@boatnet/bn-couch';
+import { Client, CouchDoc, ListOptions } from 'davenport';
+import { getSelections, getOracleWaivers, getFishTicket } from '@boatnet/bn-common';
+
+import { WatchOptions } from 'vue';
+
+import moment from 'moment';
+
+export default createComponent({
+    props: {
+        id: String,
+    },
+    setup(props, context) {
+        const store = context.root.$store;
+        const state = store.state;
+        const router = context.root.$router;
+        const ftids = props.id;
+
+        const masterDB = couchService.masterDB;
+
+        const selected: any = ref([]);
+        const pagination = {
+            // sortBy: 'period_end',
+            descending: true,
+            rowsPerPage: 15
+            };
+
+        const fishTickets: any = ref([]);
+
+        const ftLoading: any = ref(false);;
+
+        const navigateBack = () => {
+            router.back();
+        };
+
+        const formatDate = (dateTime: string) => {
+            return moment(dateTime).format('MM/DD/YYYY');
+        };
+
+        const ftColumns = [
+            {name: 'FTID', label: 'Fish Ticket #', field: 'FTID', required: true, align: 'left', sortable: true},
+            {name: 'VESSEL_NUM', label: 'Vessel ID #', field: 'VESSEL_NUM', required: true, align: 'left', sortable: true},
+            {name: 'CONDITION_CODE', label: 'Disposition', field: 'CONDITION_CODE', required: true, align: 'left', sortable: true},
+            {name: 'SPECIES_NAME', label: 'SPECIES NAME', field: 'SPECIES_NAME', required: true, align: 'left', sortable: true},
+            {name: 'LANDED_WEIGHT_LBS', label: 'Landed LBS', field: 'LANDED_WEIGHT_LBS', required: true, align: 'left', sortable: true},
+            {name: 'NUM_OF_FISH', label: 'Count', field: 'NUM_OF_FISH', required: true, align: 'left', sortable: true},
+        ];
+
+        const getSpeciesDocs = async (speciesCodes: string | number) => {
+            let speciesDocs = [];
+            try {
+                const speciesQuery = await masterDB.view(
+                    'em-views',
+                    'wcgopCode-to-pacfinCode-map',
+                    {include_docs: true, keys: speciesCodes}
+                )
+                speciesDocs = speciesQuery.rows.map((row: any) => row.doc);
+            } catch (err) {
+                console.error(err);
+            }
+            return speciesDocs;
+        }
+
+
+        const getFishTickets = async () => {
+            const ids = ftids!.split(', ');
+            for (const id of ids) {
+                const ticketRows = await getFishTicket(id);
+                const speciesDocs = await getSpeciesDocs(ticketRows.map((row: any) => row.PACFIN_SPECIES_CODE))
+                console.log(speciesDocs);
+                for (let row of ticketRows) {
+                    row.FTID = id;
+                    const species = speciesDocs.find( (speciesRow: any) => speciesRow.pacfinSpeciesCode === row.PACFIN_SPECIES_CODE );
+                    console.log(species);
+                    row.SPECIES_NAME = species ? species.commonNames[0] : '';
+                    fishTickets.value.push(row);
+                }
+            }
+        };
+
+        onMounted( async () => {
+            await getFishTickets();
+        });
+
+        return {
+            formatDate,
+            fishTickets,
+            ftids,
+            ftLoading,
+            navigateBack,
+            pagination,
+            selected,
+            ftColumns,
+        };
+    }
+});
+
+</script>
+
+<style scoped>
+.break {
+    flex-basis: 100%;
+    height: 0 !important;
+    margin: 0;
+    padding: 0;
+}
+
+.q-data-table td, .q-data-table th {
+    /* don't shorten cell contents */
+    white-space: normal !important;
+}
+</style>
--- apps/obs-web/src/views/Waivers.vue
+++ apps/obs-web/src/views/Waivers.vue
@@ -5,6 +5,8 @@
             <q-btn class="bg-primary text-white q-ma-md" @click="newWaiver">New Waiver</q-btn>
         </div>
 
+        <q-select label="Issue Year" v-model="waiverYear" :options="yearOptions"></q-select>
+
         <q-table
             :data="waivers"
             :columns="columns"
@@ -15,7 +17,6 @@
             :pagination.sync="pagination"
             :loading="loading"
             binary-state-sort
-            hide-bottom
         >
 
             <template v-slot:top>
@@ -27,15 +28,20 @@
             </template>
 
             <template v-slot:body="props">
-                <q-tr :props="props" @click.native="waiverDetails(props.row, props)">
+                <q-tr :props="props" @click.native="props.row.createdBy ? waiverDetails(props.row, props) : null">
                     <q-td key="id"></q-td>
-                    <q-td key="waiverId" :props="props">{{ props.row.waiverId }}</q-td>
-                    <q-td key="vesselName" :props="props">{{ props.row.vessel.vesselName }}</q-td>
-                    <q-td key="vesselCGNumber" :props="props">{{ (props.row.vessel.coastGuardNumber ? props.row.vessel.coastGuardNumber : props.row.vessel.stateRegulationNumber) }}</q-td>
-                    <q-td key="createdBy" :props="props">{{ formatIssuerName(props.row.createdBy) }}</q-td>
-                    <q-td key="date" :props="props">{{ formatDateTime(props.row.createdDate) }}</q-td>
-                    <q-td key="waiverType" :props="props">{{ props.row.waiverType.description }}</q-td>
-                    <q-td key="reason" :props="props">{{ props.row.reason.description }}</q-td>
+                    <q-td key="waiverId" :props="props">{{ props.row.waiverId ? props.row.waiverId : props.row.WAIVER_ID }}</q-td>
+                    <q-td key="vesselName" :props="props">{{ props.row.vessel ? props.row.vessel.vesselName : props.row.VESSEL_NAME }}</q-td>
+                    <q-td key="vesselCGNumber" :props="props">{{ (props.row.vessel ? (props.row.vessel.coastGuardNumber ? props.row.vessel.coastGuardNumber : props.row.vessel.stateRegulationNumber) : props.row.VESSEL_DRVID) }}</q-td>
+                    <q-td key="fishery" :props="props">{{ props.row.fishery ? props.row.fishery.description : props.row.FISHERY }}</q-td>
+                    <q-td key="permit" :props="props">{{ props.row.certificateNumber ? props.row.certificateNumber.permitNumber : props.row.PERMIT_OR_LICENSE }}</q-td>
+                    <q-td key="contact" :props="props">{{ props.row.contact ? (props.row.contact.firstName + ' ' + props.row.contact.lastName) : props.row.CONTACT }}</q-td>
+                    <q-td key="createdBy" :props="props">{{ props.row.createdBy ? formatIssuerName(props.row.createdBy) : 'ORACLE' }}</q-td>
+                    <q-td key="date" :props="props">{{ props.row.createdDate ? formatDateTime(props.row.createdDate) : formatDateTime(props.row.WAIVER_CREATED_DATE) }}</q-td>
+                    <q-td key="waiverType" :props="props">{{ props.row.waiverType ? props.row.waiverType.description :props.row.WAIVER_TYPE }}</q-td>
+                    <q-td key="reason" :props="props">{{ props.row.reason ? props.row.reason.description : props.row.WAIVER_REASON }}</q-td>
+                    <q-td key="notes" :props="props">{{ props.row.notes ? (props.row.notes.substring(0, 40) + (props.row.notes.length > 40 ? '...' : '') ) : props.row.NOTES ? (props.row.NOTES.substring(0, 40) + (props.row.NOTES.length > 40 ? '...' : '')) : '' }}</q-td>
+                    <q-td key="source" :props="props">{{ props.row.createdBy ? 'BOATNET' : 'ORACLE' }}</q-td>
                 </q-tr>
             </template>
 
@@ -66,6 +72,7 @@ import moment from 'moment';
 import { startCase } from 'lodash';
 import { Waiver, WaiverTypeTypeName } from '@boatnet/bn-models';
 import { AuthState, authService } from '@boatnet/bn-auth';
+import { getOracleWaivers } from '@boatnet/bn-common';
 
 export default createComponent({
     setup(props, context) {
@@ -77,6 +84,7 @@ export default createComponent({
         const pagination = {
             sortBy: 'waiverId',
             descending: true,
+            rowsPerPage: 15
             };
 
         const filterText: any = ref('');
@@ -85,18 +93,35 @@ export default createComponent({
         const waivers: any = ref([]);
         const loading: any = ref(false);
 
+        const waiverYear = ref(moment().format('YYYY'));
+
+        const yearOptions = ref([waiverYear]);
+
+        const getYearOptions = () => {
+            for (let i = 0; i < 10; i++) {
+                yearOptions.value.push((parseInt(waiverYear.value, 10) - i).toString());
+            }
+        };
+
         const columns = [
             {name: 'waiverId', label: 'Waiver ID', field: 'waiverId', required: true, align: 'left', sortable: true},
             {name: 'vesselName', label: 'Vessel Name', field: 'vesselName', required: true, align: 'left', sortable: true, sort: (a: any, b: any) => a.description > b.description },
             {name: 'vesselCGNumber', label: 'Vessel ID', field: 'vesselCGNumber', required: true,
             sortable: true, align: 'left' },
+            {name: 'fishery', label: 'Fishery', field: 'fishery', required: true, align: 'left', sortable: true},
+            {name: 'permit', label: 'Permit', field: 'permit', required: true, align: 'left', sortable: true},
+            {name: 'contact', label: 'Contact', field: 'contact', required: true, align: 'left', sortable: true},
             {name: 'createdBy', label: 'Issuer', field: 'createdBy', required: true, align: 'left', sortable: true},
             {name: 'date', label: 'Issue Date', field: 'createdDate', required: true, align: 'left', sortable: true, sort: (a: any, b: any) => (a).localeCompare(b) },
             {name: 'waiverType', label: 'Type', field: 'waiverType', required: true, align: 'left', sortable: true, sort: (a: any, b: any) => a.description > b.description},
             {name: 'reason', label: 'Reason', field: 'reason', required: true, align: 'left', sortable: true, sort: (a: any, b: any) => a.description > b.description },
+            {name: 'notes', label: 'Notes', field: 'notes', required: true, align: 'left', sortable: false},
+            {name: 'source', label: 'Source', field: 'source', required: true, align: 'left', sortable: 'true'}
         ];
 
         const getWaivers = async () => {
+            allWaivers.value.length = 0;
+            waivers.value.length = 0;
             const masterDB: Client<any> = couchService.masterDB;
             const queryOptions: any = {
                 include_docs: true,
@@ -112,14 +137,17 @@ export default createComponent({
                     queryOptions
                 );
 
-                allWaivers.value = waiversQuery.rows.map( (row: any) => row.doc);
-                waivers.value = waiversQuery.rows.map( (row: any) => row.doc);
+                allWaivers.value = waiversQuery.rows.filter((row: any) => moment(row.doc.createdDate).format('YYYY') === waiverYear.value).map( (row: any) => row.doc);
+                waivers.value = waiversQuery.rows.filter((row: any) => moment(row.doc.createdDate).format('YYYY') === waiverYear.value).map( (row: any) => row.doc);
 
                 loading.value = false;
 
             } catch (err) {
                 console.log(err);
             }
+            const oracleWaivers = await getOracleWaivers(waiverYear.value);
+            allWaivers.value.push.apply(allWaivers.value, oracleWaivers);
+            waivers.value.push.apply(waivers.value, oracleWaivers);
         };
 
         const formatDateTime = (dateTime: string) => {
@@ -131,7 +159,7 @@ export default createComponent({
         };
 
         onMounted( async () => {
-            await getWaivers();
+            getYearOptions();
         });
 
         const waiverDetails = async (waiver: any) => {
@@ -153,19 +181,39 @@ export default createComponent({
                     const needle = newVal.toLowerCase();
                     let waiverResults: any = [];
                     waiverResults = allWaivers.value.filter( (row: any) => {
-                        return row.vessel.vesselName.toLowerCase().includes(needle) ||
-                                (row.vessel.coastGuardNumber ? row.vessel.coastGuardNumber : row.vessel.stateRegulationNumber).toLowerCase().includes(needle) ||
-                                row.createdBy.toLowerCase().includes(needle) ||
-                                row.createdDate.includes(needle) ||
-                                row.waiverType.description.toLowerCase().includes(needle) ||
-                                row.reason.description.toLowerCase().includes(needle) ||
-                                row.waiverId.includes(needle);
+                        return row.vessel && row.vessel.vesselName.toLowerCase().includes(needle) ||
+                                row.vessel && (row.vessel.coastGuardNumber ? row.vessel.coastGuardNumber : row.vessel.stateRegulationNumber).toLowerCase().includes(needle) ||
+                                row.fishery && row.fishery.description.toLowerCase().includes(needle) ||
+                                row.certificateNumber && row.certificateNumber.permitNumber.toLowerCase().includes(needle) ||
+                                row.createdBy && row.createdBy.toLowerCase().includes(needle) ||
+                                row.createdDate && row.createdDate.includes(needle) ||
+                                row.waiverType && row.waiverType.description.toLowerCase().includes(needle) ||
+                                row.reason && row.reason.description.toLowerCase().includes(needle) ||
+                                row.waiverId && row.waiverId.includes(needle) ||
+                                row.VESSEL_NAME && row.VESSEL_NAME.toLowerCase().includes(needle) ||
+                                row.VESSEL_DRVID && row.VESSEL_DRVID.toLowerCase().includes(needle) ||
+                                row.WAIVER_CREATED_DATE && row.WAIVER_CREATED_DATE.includes(needle) ||
+                                row.WAIVER_TYPE && row.WAIVER_TYPE.toLowerCase().includes(needle) ||
+                                row.WAIVER_REASON && row.WAIVER_REASON.toLowerCase().includes(needle) ||
+                                row.FISHERY && row.FISHERY.toLowerCase().includes(needle) ||
+                                row.WAIVER_ID && row.WAIVER_ID.toString().includes(needle) ||
+                                row.PERMIT_OR_LICENSE && row.PERMIT_OR_LICENSE.toLowerCase().includes(needle);
                     });
                     waivers.value.length = 0;
                     waivers.value.push.apply(waivers.value, waiverResults);
                 } else {
                     waivers.value.length = 0;
                     waivers.value.push.apply(waivers.value, allWaivers.value);
+                }
+            },
+            watcherOptions
+        );
+
+        watch(
+            () => waiverYear.value,
+            async (newVal, oldVal) => {
+                if (newVal !== '') {
+                    await getWaivers();
                 }
             },
             watcherOptions
@@ -182,7 +230,9 @@ export default createComponent({
             pagination,
             selected,
             waiverDetails,
-            waivers
+            waivers,
+            waiverYear,
+            yearOptions
         };
     }
 });
